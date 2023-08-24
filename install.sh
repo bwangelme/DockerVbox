59692bb0b93c782d13dff897c17f69ffb3d07ee2
bwangelme <bwangel.me@gmail.com>
2023-08-24 09:48:03 +0800

    update

--- install.sh
+++ install.sh
@@ -4,5 +4,12 @@
 # Date:8月,22,2023 20:30
 
 
-cp /vagrant/vimrc /root/.vimrc
-apt-get update && apt-get install net-tools ripgrep
+# cp /vagrant/vimrc /root/.vimrc
+
+function change_apt_source() {
+    echo "修改 apt 源为 ustc"
+    sed -i "s/archive.ubuntu.com/mirrors.ustc.edu.cn/g" /etc/apt/sources.list
+}
+
+apt-get update && apt-get install -y net-tools ripgrep nfs-kernel-server
+
